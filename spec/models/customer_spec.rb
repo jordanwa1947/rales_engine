f5b21660dc59456e809c66947b2314934e813441
Jordan Whitten <jordanwa1947@gmail.com>
2018-11-28 00:13:41 -0700

    added changes to factory bot

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
   describe 'Relationships' do
-    it should { have_many :invoices }
-    it should { have_many(:merchants).through(:invoices)}
+    it { should have_many :invoices }
+    it { should have_many(:merchants).through(:invoices)}
   end
 end
