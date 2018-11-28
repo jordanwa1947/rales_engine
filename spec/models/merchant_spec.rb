6e3de00d9f85b60d172c8a8ff4b320c60d09a690
Jordan Whitten <jordanwa1947@gmail.com>
2018-11-27 23:39:32 -0700

    added validations for models

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -2,8 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
   describe 'Relationships' do
-    it  { should have_many :invoices}
-    it  { should have_many(:customers).through(:invoices)}
+    it should { have_many :invoices}
+    it should { have_many(:customers).through(:invoices)}
   end
-
 end
