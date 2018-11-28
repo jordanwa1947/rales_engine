b558e4883075fb5e17fd58dc5bcf4f99ff858202
Jordan Whitten <jordanwa1947@gmail.com>
2018-11-27 23:07:51 -0700

    fixed pending tests issues with syntax issue

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Invoice, type: :model do
   describe 'Relationships' do
-    it should { have_many :transactions }
-    it should { have_many :invoice_items}
-    it should { have_many(:items).through(:invoice_items)}
+    it  { should have_many :transactions }
+    it  { should have_many :invoice_items}
+    it  { should have_many(:items).through(:invoice_items)}
   end
 end
