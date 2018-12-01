648d595f3bfd9bd30963d1827371343bd396a209
Jordan Whitten <jordanwa1947@gmail.com>
2018-11-30 20:44:26 -0700

    invoice index request and serializer

--- app/controllers/api/v1/invoices_controller.rb
+++ app/controllers/api/v1/invoices_controller.rb
@@ -1,3 +1,6 @@
 class Api::V1::InvoicesController < ApplicationController
 
+  def index
+    render json: InvoiceSerializer.new(Invoice.all).serializable_hash
+  end 
 end
---
+++ spec/requests/api/v1/invoice_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe 'Invoice API' do
+  it 'sends a list of invoices' do
+    create_list(:invoice, 3)
+
+    get '/api/v1/invoices'
+
+    expect(response).to be_successful
+
+    invoices = JSON.parse(response.body)
+    expect(invoices["data"].count).to eq(3)
+  end
+end
