e6bdde962851dd72659e33eb9c99114007597907
Jordan Whitten <jordanwa1947@gmail.com>
2018-12-02 12:51:23 -0700

    invoice association controller for and show action for transactions

--- config/routes.rb
+++ config/routes.rb
@@ -28,6 +28,7 @@ Rails.application.routes.draw do
         get '/',to: 'transactions#index'
         get '/find', to: 'find#show'
         get '/find_all', to: 'find#index'
+        get '/:id/invoice', to: 'invoice_association#show'
       end
     end
   end
